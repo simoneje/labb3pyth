cbbe913819bde1a7d970685f0466dee22cfbf36e
simoneje <58808601+simoneje@users.noreply.github.com>
2020-02-05 23:19:17 +0100

    Add files via upload

--- pythonlabb3/classes.py
+++ pythonlabb3/classes.py
@@ -1,68 +1,92 @@
 import requests
 import json
+import sys
+sokTermer = []
+savedVal = []
 class meny():
-
-    def __init__(self):
-    
+    def __init__(self):   
         def Meny(self):
             choice = int(input('1)Välj film\n2)Visa senaste sökningar\n3)Avsluta\n'))
             while True:
                 if choice == 1:
-                    MovieDB()                 
+                    MovieDB()
+                    Meny(self)                 
                     break
                 elif choice == 2:
-                    print('1)Visa information om senaste sökningar\n2)Visa information om vald sökt film\n')
+                    print(sokTermer)
+                    DoubleMeny()
+                    Meny(self)
+                    #print('1)Visa information om senaste sökningar\n2)Visa information om vald sökt film\n')
                     break
                 elif choice == 3:
-                    break
-        
-    
+                    sys.exit()
+                    break    
         Meny(self)
 
 class MovieDB():
+    global sokTermer
+    global savedVal
     def __init__(self):
         def SearchFilm(self):
-            iput = input('Sök\n')
-            
-            search = requests.get(f'http://www.omdbapi.com/?i=tt3896198&apikey=3dde47b4&s={iput}')
-            data = search.json()
-            try: 
-                with open('hej.json', 'w', encoding="utf-8") as big:
-                    json.dump(data, big, ensure_ascii=False, indent=2)
-            except FileNotFoundError as error:
-                print(error)
-            lol = data['Search']
-            count = 0
-            for i in lol:
-                count += 1
-                print(count,i['Title'],i['Year'])
-            
-            nsearch = input('Skriv exakt vilken title du menar')
-            for u in range(len(lol)):
-                
-                if nsearch == lol[u]['Title']:
-                    try:
-                        with open('savedsearch.json', 'w', encoding="utf-8") as jobb:
-                            json.dump(lol[u], jobb, ensure_ascii=False, indent=2)
-                    except FileNotFoundError:
-                        print(FileNotFoundError)
-                    print(lol[u])
+            try:
+                iput = input('Sök\n')
+                sokTermer.append(iput)
+                try:
+                    search = requests.get(f'http://www.omdbapi.com/?i=tt3896198&apikey=3dde47b4&s={iput}')
+                    data = search.json()
+                except: print('oj')
+                try: 
+                    with open('hej.json', 'w', encoding="utf-8") as big:
+                        json.dump(data, big, ensure_ascii=False, indent=2)
+                except FileNotFoundError as error:
+                    print(error)
+                lol = data['Search']
+                count = 0
+                for i in lol:
+                    count +=1
+                    print(count,')',i['Title'],i['Year'])                            
+                nsearch = int(input('Skriv siffran på filmen du vill ha\n'))
+                try:
+                    with open('hej.json', 'r', encoding="utf-8") as jsonfil1:
+                        searchRes = json.load(jsonfil1)
+                        filmVal = searchRes.get('Search')[nsearch-1]
+                        savedVal.append(filmVal)
+                except FileNotFoundError:
+                    print(FileNotFoundError)
+                try:
+                    with open('savedsearch.json', 'w', encoding="utf-8") as jsonfil2:
+                        json.dump(filmVal, jsonfil2, ensure_ascii=False, indent=2)
+                except FileNotFoundError:
+                    print(FileNotFoundError)
+            except: print('försök igen')                       
+        SearchFilm(self)    
 
-                
-            
-            
-        SearchFilm(self)
+class LastSearch():
+    def __init__(self):
+        def serHist(self):
+            with open('savedsearch.json', 'r', encoding='utf-8') as file_read:
+                p = json.load(file_read)
+                print(p)
         
-        #def SortFilm(self, filename):
-            #newlist = [] 
-            #listig = []          
-            #try:
-            #    with open('hej.json', encoding="utf-8") as sJson:
-            #        newlist = json.load(sJson)                                    
-            #except FileNotFoundError as error:
-            #    print(error)
+        serHist(self)
 
 
+class DoubleMeny():    
+    def __init__(self):
+        def menyTva(self):
+            choice = int(input('1)Visa info om senaste sökningar\n2)Visa info om senast vald sökning\n3)Gå tillbaka\n'))
+            while True:
+                if choice == 1:
+                    print(savedVal)
+                    menyTva(self)                 
+                    break
+                elif choice == 2:
+                    LastSearch()
+                    menyTva(self)
+                    break
+                elif choice == 3:
+                    meny()
+        menyTva(self)
 
 
 
